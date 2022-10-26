cd6391850a59a98af5067246930c46bee36eb239
Ioana2002 <maria.antohi@stud.ubbcluj.ro>
2022-10-26 16:08:37 +0300

    Add project files.

---
+++ Ioana_Antohi_Lab2/Data/Ioana_Antohi_Lab2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ioana_Antohi_Lab2.Models;
+
+namespace Ioana_Antohi_Lab2.Data
+{
+    public class Ioana_Antohi_Lab2Context : DbContext
+    {
+        public Ioana_Antohi_Lab2Context (DbContextOptions<Ioana_Antohi_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ioana_Antohi_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Ioana_Antohi_Lab2.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Ioana_Antohi_Lab2.Models.Author> Author { get; set; }
+    }
+}
