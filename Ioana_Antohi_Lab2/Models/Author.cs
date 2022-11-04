2d3031bd016a485299bf2e4ead451703fff6a89d
Ioana2002 <maria.antohi@stud.ubbcluj.ro>
2022-11-05 00:07:22 +0200

    Laborator 4

--- Ioana_Antohi_Lab2/Models/Author.cs
+++ Ioana_Antohi_Lab2/Models/Author.cs
@@ -1,10 +1,22 @@
-﻿namespace Ioana_Antohi_Lab2.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Ioana_Antohi_Lab2.Models
 {
     public class Author
     {
         public int ID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
         public ICollection<Book>? Books { get; set; }
 
     }
---
+++ Ioana_Antohi_Lab2/Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,11 @@
+﻿using Ioana_Antohi_Lab2.Models;
+
+namespace Ioana_Antohi_Lab2.Models.ViewModels
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+
+    }
+}
